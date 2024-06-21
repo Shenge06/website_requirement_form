e11117a7f698d74ff5f6de7692e94baf13186ac3
Nkululeko Buthelezi <143996288+Shenge06@users.noreply.github.com>
2024-06-21 23:33:58 +0200

    Delete website_requirement_form directory

--- website_requirement_form/requirement_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RequirementAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'requirement_app'
--- website_requirement_form/requirement_app/urls.py
+++
@@ -1,8 +0,0 @@
-# requirement_app/urls.py
-from django.urls import path
-from .views import requirement_form_view
-
-urlpatterns = [
-    path('', requirement_form_view, name='requirement_form'),
-]
-
